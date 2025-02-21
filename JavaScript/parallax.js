a1ab20f3608ba38c6c25b9aa8551817e339ae775
VithorRoder <vithvith22@gmail.com>
2025-02-21 13:26:13 -0300

    cutback

--- JavaScript/parallax.js
+++
@@ -1,4 +0,0 @@
-window.addEventListener("scroll", function () {
-    let scrolled = window.scrollY;
-    document.querySelector(".parallax").style.transform = `translateY(${scrolled * 0.5}px)`;
-});
