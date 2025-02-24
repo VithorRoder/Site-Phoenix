bce3b38f9b44ed397a237c8bb63f1cfef98dd361
VithorRoder <vithvith22@gmail.com>
2025-02-24 15:23:09 -0300

    nova parte da pagina

--- JavaScript/swiper.js
+++ JavaScript/swiper.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     var swiper = new Swiper(".swiper-container", {
         loop: true,
         autoplay: {
-            delay: 4000,
+            delay: 3000,
             disableOnInteraction: false,
         },
         navigation: {
