1a8d4c090ba1ef840c119a69a8711ecdc1129adc
VithorRoder <vithvith22@gmail.com>
2025-02-24 13:28:59 -0300

    ta vindo mlk

--- JavaScript/lilSquares.js
+++ JavaScript/lilSquares.js
@@ -1,13 +1,15 @@
 const nomesCategorias = [
-    ["João", "Estela", "Carlos", "Mariana", "Lucas", "Fernanda", "Pedro", "Sofia", "Rafael"], 
-    ["Blocos", "Bruno", "Clara", "Daniel", "Eduarda", "Felipe", "Gabriel", "Helena", "Igor"],   
-    ["Juliana", "Kevin", "Larissa", "Miguel", "Natália", "Otávio", "Priscila", "Rodrigo", "Samuel"]
+    ["Vinil Branco", "Vinil Transparente", "Plantas de Terreno", "Vinil com Recorte", "Lona", "Banner", "Perfurado", "Placas Adesivadas"], 
+    ["Credenciais", "Convites", "Apostilas", "Calendários", "Agendas", "Livros", "Rótulos", "Fotos"],   
+    ["Panfletos", "Folders", "Cartão de Visita", "Pastas", "Blocos", "Receituários", "Folhetos", "Material de Campanha"],
+    ["Camisas", "Canecas", "Pulseiras", "Crachá em PVC", "Ecobag", "Wind Banner", "Caixas", "Display em Acrílico"]
 ];
 
 const imagensCategorias = [
-    ["Images/Joao.jpg", "img/estela.jpg", "img/carlos.jpg", "img/mariana.jpg", "img/lucas.jpg", "img/fernanda.jpg", "img/pedro.jpg", "img/sofia.jpg", "img/rafael.jpg"],
-    ["Images/bloco.jpg", "img/bruno.jpg", "img/clara.jpg", "img/daniel.jpg", "img/eduarda.jpg", "img/felipe.jpg", "img/gabriel.jpg", "img/helena.jpg", "img/igor.jpg"],
-    ["img/juliana.jpg", "img/kevin.jpg", "img/larissa.jpg", "img/miguel.jpg", "img/natalia.jpg", "img/otavio.jpg", "img/priscila.jpg", "img/rodrigo.jpg", "img/samuel.jpg"]
+    ["Images/Joao.jpg", "img/estela.jpg", "img/carlos.jpg", "img/mariana.jpg", "img/lucas.jpg", "img/fernanda.jpg", "img/pedro.jpg", "img/sofia.jpg"],
+    ["Images/credenciais.jpg", "img/bruno.jpg", "img/clara.jpg", "img/daniel.jpg", "img/eduarda.jpg", "img/felipe.jpg", "img/gabriel.jpg", "img/helena.jpg"],
+    ["img/juliana.jpg", "img/kevin.jpg", "img/larissa.jpg", "img/miguel.jpg", "Images/bloco.jpg", "img/otavio.jpg", "img/priscila.jpg", "img/rodrigo.jpg"],
+    ["img/juliana.jpg", "img/kevin.jpg", "img/larissa.jpg", "img/miguel.jpg", "img/natalia.jpg", "img/otavio.jpg", "img/priscila.jpg", "img/rodrigo.jpg"]
 ];
 
 const grids = document.querySelectorAll(".square-grid");
