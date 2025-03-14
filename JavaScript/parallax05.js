de60d1587746e71ed9a7fdf72852d3186ef8ec46
VithorRoder <vithvith22@gmail.com>
2025-03-14 16:46:54 -0300

    bora

--- JavaScript/parallax05.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("scroll", function () {
-    let scrollPos = window.scrollY;
-    document.querySelector(".parallax05 img").style.transform = `translateY(${scrollPos * 0.3}px)`;
-});
